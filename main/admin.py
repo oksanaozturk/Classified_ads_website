08d261edd46063d648f0d5d12e95ccd0c21fa531
casper <oksanaozturk@gmail.com>
2024-06-29 22:07:45 +0300

    Настройка откажения объектов моделей Ad and Comment в Админке

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import Ad, Comment
+
+
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    """
+    Класс для регистрации модели Ad в Админке."""
+
+    list_display = (
+        "id",
+        "title",
+        "price",
+        "description",
+        "author",
+        "created_at",
+        "image",
+    )
+    list_filter = ("created_at",)
+    search_fields = ("title", "author")
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Класс для регистрации модели Comment в Админке."""
+
+    list_display = (
+        "id",
+        "text",
+        "author",
+        "ad",
+        "created_at",
+    )
+    list_filter = ("created_at",)
+    search_fields = ("ad", "author")
