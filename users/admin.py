a1bd6f2539b31843b759979c8614cc2983b704bf
casper <oksanaozturk@gmail.com>
2024-06-29 15:42:00 +0300

    Настройка отображения объектов модели User в Админке

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Класс для регистрации User в админке."""
+
+    list_display = (
+        "id",
+        "email",
+        "is_active",
+        "password",
+        "first_name",
+        "last_name",
+        "phone_number",
+        "avatar",
+        "role",
+    )
+    list_filter = ("email",)
+    search_fields = ("email", "last_name", "phone_number")
+
+
+from django.contrib import admin
+
 # Register your models here.
