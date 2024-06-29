0aaaf348828a328ceed2ae39a0a76f37a3db169c
casper <oksanaozturk@gmail.com>
2024-06-29 22:05:50 +0300

    Реализация путей для моделей Ad and Comments

---
+++ main/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+# Используем SimpleRouter так как с ним можно создать несколько экземпляров класса, а с DefaultRouter только один
+from rest_framework.routers import SimpleRouter
+
+from main.apps import MainConfig
+from main.views import AdViewSet, CommentViewSet, MyListAPIView
+
+app_name = MainConfig.name
+# Создаем экземпляр класса SimpleRouter(), он обеспечивает маршрутизацию всех путей CRUD
+router = SimpleRouter()
+router.register("ads", AdViewSet, basename="ads")
+"""
+GET "/main/ads/" — список профилей пользователей
+POST "/main/ads/" — регистрация пользователя
+GET, PATCH, DELETE "/main/ads/{id}" — в соотвествии с REST и необходимыми permissions (для администратора)
+
+GET "/main/comments/" — список профилей пользователей
+POST "/main/comments/" — регистрация пользователя
+GET, PATCH, DELETE "/main/comments/{id}" — в соотвествии с REST и необходимыми permissions (для администратора)
+"""
+router.register("comments", CommentViewSet, basename="comments")
+
+urlpatterns = [
+    path("mylist/", MyListAPIView.as_view(), name="mylist"),
+
+] + router.urls
+
