6171351a1ca292a5357886c4f04f10e601d2350c
casper <oksanaozturk@gmail.com>
2024-06-29 15:36:46 +0300

    Создание сериализаторов к модели User

---
+++ users/serializers.py
@@ -0,0 +1,25 @@
+from djoser.serializers import UserCreateSerializer as BaseUserRegistrationSerializer
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserRegistrationSerializer(BaseUserRegistrationSerializer):
+    """
+    Класс сериализатора при регистрации Пользователя (модель User).
+    """
+    password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = "__all__"
+
+
+class CurrentUserSerializer(serializers.ModelSerializer):
+    """
+    Класс сериализатора для текущего отображения Пользователя (модель User).
+    """
+    class Meta:
+        model = User
+        fields = ('email', 'role',  'first_name', 'last_name', 'password', 'phone_number', 'avatar')
