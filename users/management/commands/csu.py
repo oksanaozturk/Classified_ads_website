60eea982fb061b3bd212f482e265266fb7769c41
casper <oksanaozturk@gmail.com>
2024-06-29 15:41:08 +0300

    Добавлена команда для создания superuser

---
+++ users/management/commands/csu.py
@@ -0,0 +1,24 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    """Класс для создания superuser, он нужен когда сломалась работа createsuperuser после нашего переопределения
+    в модели User username"""
+
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email="admin1@sky.pro",
+            # Поля, которые разрешат вход в Админку
+            first_name='admin_main',
+            last_name='custom',
+            role='admin',
+            is_active=True,
+        )
+        # Функция set_password() нужна для кеширования пароля
+        user.set_password("123qwe")
+        # Функцмя save() нужна для сохранения пользователя
+        user.save()
+
+        print(f"Создание superuser  прошло успешно: {user.email} пароль: 123qwe")
